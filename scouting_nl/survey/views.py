64e847cf26937c015c904d1ad8c3cd3fc08085c6
Max van Gorp <534253@student.glu.nl>
2021-05-31 16:25:32 +0200

    first commit

--- scouting_nl/survey/views.py
+++ scouting_nl/survey/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Questionnaire, Question, Answer
 from django.forms import forms
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator
 
 def index(request):
     all_questionnaires = Questionnaire.objects.all()
@@ -14,5 +14,5 @@ def questionnaire_detail(request, questionnaire_id):
     questionnaire = Questionnaire.objects.get(id=questionnaire_id)
     questions = Question.objects.filter(questionnaire=questionnaire)
     return render(request, 'questions/questionnaire.html',
-                  {'questionnaire': questionnaire, 'questions': questions})
+                  {'questionnaire': questionnaire, 'questions': questions,})
 
